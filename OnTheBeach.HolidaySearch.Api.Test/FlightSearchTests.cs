1f001a9d74baadbcb1b89e0d9ce28e543842ee95
Conrad Hill <conradh@gmail.com>
2022-05-09 23:08:20 +0100

    Handle London airports and test of the search for customer 2

--- OnTheBeach.HolidaySearch.Api.Test/FlightSearchTests.cs
+++ OnTheBeach.HolidaySearch.Api.Test/FlightSearchTests.cs
@@ -37,5 +37,34 @@ namespace OnTheBeach.HolidaySearch.Api.Test
 
             Assert.Equal(2, result.FirstOrDefault().Id);
         }
+
+        [Fact]
+        public void Customer2_TestFlightSearchWorks()
+        {
+            /*
+             * Departing from: Manchester Airport (MAN)
+                Traveling to: Malaga Airport (AGP)
+                Departure Date: 2023/07/01
+                Duration: 7 nights
+
+                Flight 2
+             */
+
+            var flightData = new Data<Flight>();
+
+            var flighSearch = new FlightSearch(flightData);
+
+            var flightCriteria = new HolidayCriteria
+            {
+                DepartingFrom = "ANY LON",
+                TravellingTo = "PMI",
+                DepartureDate = DateTime.Parse("2023-06-15"),
+                Duration = 10
+            };
+
+            var result = flighSearch.Search(flightCriteria);
+
+            Assert.Equal(6, result.FirstOrDefault().Id);
+        }
     }
 }
--- OnTheBeachHolidayApi/Search/FlightSearch.cs
+++ OnTheBeachHolidayApi/Search/FlightSearch.cs
@@ -6,6 +6,9 @@ namespace OnTheBeach.HolidaySearch.Api
 {
     public class FlightSearch
     {
+
+        private List<string> _londonAirports = new List<string> { "LCY", "LHR", "LGW", "LTN", "STN", "SEN" };
+
         private readonly Data<Flight> _flights;
 
         public FlightSearch(Data<Flight> flights)
@@ -18,10 +21,19 @@ namespace OnTheBeach.HolidaySearch.Api
         {
             var flights = _flights.GetData().Where(
                     x => x.To == criteria.TravellingTo
-                    && x.From == criteria.DepartingFrom
+                    && CheckAirport(x, criteria.DepartingFrom)
                     && x.DepartureDate == criteria.DepartureDate
                     ).OrderBy(o => o.Price).ToList();
             return flights;
+        }
+
+        private bool CheckAirport(Flight flight, string DepartingFrom)
+        {
+            if(DepartingFrom == "ANY LON")
+            {
+                return _londonAirports.Contains(flight.From);
+            }
+            return flight.From == DepartingFrom;
         }
     }
 }
